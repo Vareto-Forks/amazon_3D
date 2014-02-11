17b2df265708eaa73ff3adbb9617e940d82531a2
Andrew Giel <agiel@stanford.edu>
2014-02-10 20:37:20 -0800

    added shoe pictures

---
+++ src/createCorrespondences.py
@@ -0,0 +1,12 @@
+import opencv as cv
+
+def getxy(event, x, y, flags, param):
+	if event == cv2.EVENT_LBUTTONDOWN:
+		print x, y
+
+img = cv.imread('../data/shoe/shoe-1.jpg')
+cv.namedWindow('image')
+cv.setCallback('image', getxy)
+cv.imshow('image', img)
+cv.waitKey(0)
+cv.destroyAllWindows()
