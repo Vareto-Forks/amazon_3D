4eb512153cf7c5f5cd39223d55f1e5d0a88dcbdc
Andrew Giel <agiel@stanford.edu>
2014-02-12 22:31:53 -0800

    updated norm8 to plot epipolars
    also added bugatti correspondences

--- src/norm8Point.py
+++ src/norm8Point.py
@@ -1,35 +1,38 @@
 import numpy as np
 import math
+import re
+import cv2 as cv 
 
 PATH_TO_DATA = '../data/'
 
-def normalize(points):
-	x1_avg = float(sum(x[0] for x in points))/(len(points))
-	y1_avg = float(sum(x[1] for x in points))/(len(points))
-	x2_avg = float(sum(x[2] for x in points))/(len(points))
-	y2_avg = float(sum(x[3] for x in points))/(len(points))
+def normalize(points1, points):
+	x1_avg = float(sum(x[0] for x in points1))/(len(points1))
+	y1_avg = float(sum(x[1] for x in points1))/(len(points1))
+	x2_avg = float(sum(x[0] for x in points2))/(len(points2))
+	y2_avg = float(sum(x[1] for x in points2))/(len(points2))
 	RMS1 = 0.0
 	RMS2 = 0.0
-	for x in points:
+	for x in points1:
 		RMS1 += math.sqrt(math.pow(x[0]-x1_avg, 2) + math.pow(x[1]-y1_avg, 2))
-		RMS2 += math.sqrt(math.pow(x[2]-x2_avg, 2) + math.pow(x[3]-y2_avg, 2))
-	RMS1 = float(RMS1)/len(points)
-	RMS2 = float(RMS2)/len(points)
+	for x in points2:
+		RMS2 += math.sqrt(math.pow(x[0]-x2_avg, 2) + math.pow(x[1]-y2_avg, 2))
+	RMS1 = float(RMS1)/len(points1)
+	RMS2 = float(RMS2)/len(points2)
 	s1 = math.sqrt(2)/RMS1
 	s2 = math.sqrt(2)/RMS2
 	T1 = np.matrix([[s1, 0, s1*x1_avg],[0, s1, s1*y1_avg],[0, 0, 1]])
 	T2 = np.matrix([[s2, 0, s2*x2_avg],[0, s2, s2*y2_avg],[0, 0, 1]])
-	points1 = []
-	for x in points:
-		points1.append([x[0], x[1], 1])
-	points2 = []
-	for x in points:
-		points2.append([x[2], x[3], 1])
-	points1 = np.matrix(points1)
-	points2 = np.matrix(points2)
-	newPoints1 = points1*T1
-	newPoints2 = points2*T2
-	return newPoints1, newPoints2
+	homoPoints1 = []
+	for x in points1:
+		homoPoints1.append([x[0], x[1], 1])
+	homoPoints2 = []
+	for x in points2:
+		homoPoints2.append([x[0], x[1], 1])
+	homoPoints1 = np.matrix(homoPoints1)
+	homoPoints2 = np.matrix(homoPoints2)
+	newPoints1 = homoPoints1*T1
+	newPoints2 = homoPoints2*T2
+	return newPoints1, newPoints2, T1, T2
 
 def constructW(A, B):
 	W = []
@@ -41,19 +44,71 @@ def constructW(A, B):
 	W = np.matrix(W)
 	return W 
 
-cFilename = raw_input('Give the name of the correspondece file from /data/ :')
+def getImages(cFilename, points1, points2):
+	folderName = re.findall('(\w+/)\w+', cFilename)[0]
+	img1Name = re.findall('(\w+)_to_\w+', cFilename)[0]
+	img2Name = re.findall('\w+_to_(\w+)', cFilename)[0]
+	img1 = cv.imread(PATH_TO_DATA+folderName+img1Name+'.jpg')
+	img2 = cv.imread(PATH_TO_DATA+folderName+img2Name+'.jpg')
+	cv.namedWindow('IMAGE 1')
+	cv.namedWindow('IMAGE 2')
+	for point in points1:
+		pt = (int(point[0]), int(point[1]))
+		cv.circle(img1, pt, 2, (255, 255, 0), -1)
+	for point in points2:
+		pt = (int(point[0]), int(point[1]))
+		cv.circle(img2, pt, 2, (255, 255, 0), -1)
+	return img1, img2
+
+def drawEpipolars(F, points1, points2, img):
+	for index, point in enumerate(points1):
+		p = np.array((point[0], point[1], 1.0))
+		pPrime = np.array((points2[index][0], points2[index][1], 1.0))
+		d = F.dot(np.transpose(p))
+		dA = d.item(0)
+		dB = d.item(1)
+		dC = d.item(2)
+
+		x = pPrime.item(0)
+		y = pPrime.item(1)
+
+		x1 = int(x-10)
+		x2 = int(x+10)
+
+		y1 = int(float(-dA*(x1)-dC)/dB)
+		y2 = int(float(-dA*(x2)-dC)/dB)
+
+		print x1, y1, x2, y2
+
+		cv.line(img, (x1, y1), (x2, y2), (255, 255, 0), 4)
+
+
+cFilename = raw_input('Give the name of the correspondece file from /data/ : ')
 cFile = open(PATH_TO_DATA + cFilename, 'r')
-points = []
+points1 = []
+points2 = []
 for line in cFile:
 	x1, y1, x2, y2 = line.split()
-	points.append([float(x1), float(y1), float(x2), float(y2)])
-
-points1, points2 = normalize(points)
-W = constructW(points1, points2)
+	points1.append([float(x1), float(y1)])
+	points2.append([float(x2), float(y2)])
+img1, img2 = getImages(cFilename, points1, points2)
+normPoints1, normPoints2, T1, T2 = normalize(points1, points2)
+W = constructW(normPoints1, normPoints2)
 U, D, V = np.linalg.svd(W)
 f = V[:, 8]
 f = f.reshape(3,3)
 U, D, V = np.linalg.svd(f)
 D.itemset(2, 0.0)
 F = U*np.diag(D)*V
+F = np.transpose(T2)*F*T1
 print F
+print 'img2'
+drawEpipolars(F, points1, points2, img2)
+print 'img1'
+drawEpipolars(np.transpose(F), points2, points1, img1)
+cv.imshow('IMAGE 1', img1)
+cv.imshow('IMAGE 2', img2)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+
