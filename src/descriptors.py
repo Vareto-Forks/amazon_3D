27ad83ab9cf2589cf4eb07ada8a879d4cdedbd30
Andrew Giel <agiel@stanford.edu>
2014-03-02 15:54:35 -0800

    added descriptors creator

---
+++ src/descriptors.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+import re
+import numpy as np
+
+PATH_TO_DATA = '../data/'
+
+def createDescriptors(filename):
+	img = cv.imread(PATH_TO_DATA+filename)
+	sift = cv.SIFT()
+	kp, des = sift.detectAndCompute(img, None)
+	folder = re.findall('(.*)/.*\.jpg', filename)[0]
+	imgName = re.findall('.*/(.*)\.jpg', filename)[0]
+	toWriteName = imgName + '.descriptors'
+	fileToWrite = open(PATH_TO_DATA + folder + "/" + toWriteName, 'w')
+	for index, d in enumerate(des):
+		kpi = kp[index]
+		kpList = [str(kpi.pt[0]), str(kpi.pt[1]), str(kpi.angle), str(kpi.size)]
+		toWrite = ", ".join(kpList) + ':'
+		for dimension in d:
+			toWrite += str(dimension) + ','
+		toWrite += '\n'	
+		fileToWrite.write(toWrite)
+	return kp, des
