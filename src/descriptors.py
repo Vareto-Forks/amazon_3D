86624b2942465f7dace4490bcefc3d6815c91514
Andrew Giel <agiel@stanford.edu>
2014-03-12 15:41:12 -0700

    added surf

--- src/descriptors.py
+++ src/descriptors.py
@@ -4,14 +4,33 @@ import numpy as np
 
 PATH_TO_DATA = '../data/'
 
-def createDescriptors(filename):
+def sift(filename):
 	img = cv.imread(PATH_TO_DATA+filename)
 	sift = cv.SIFT()
 	gray= cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 	kp, des = sift.detectAndCompute(gray, None)
 	folder = re.findall('(.*)/.*\.jpg', filename)[0]
 	imgName = re.findall('.*/(.*)\.jpg', filename)[0]
-	toWriteName = imgName + '.descriptors'
+	toWriteName = imgName + '.sift'
+	fileToWrite = open(PATH_TO_DATA + folder + "/" + toWriteName, 'w')
+	for index, d in enumerate(des):
+		kpi = kp[index]
+		kpList = [str(kpi.pt[0]), str(kpi.pt[1]), str(kpi.angle), str(kpi.size)]
+		toWrite = ", ".join(kpList) + ':'
+		for dimension in d:
+			toWrite += str(dimension) + ','
+		toWrite += '\n'	
+		fileToWrite.write(toWrite)
+	return kp, des
+
+def surf(filename):
+	img = cv.imread(PATH_TO_DATA+filename)
+	surf = cv.SURF()
+	gray= cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+	kp, des = surf.detectAndCompute(gray, None)
+	folder = re.findall('(.*)/.*\.jpg', filename)[0]
+	imgName = re.findall('.*/(.*)\.jpg', filename)[0]
+	toWriteName = imgName + '.surf'
 	fileToWrite = open(PATH_TO_DATA + folder + "/" + toWriteName, 'w')
 	for index, d in enumerate(des):
 		kpi = kp[index]
