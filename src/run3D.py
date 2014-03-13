d5b1f7133151aba28edc09c589450e4e3e2e5744
Andrew Giel <agiel@stanford.edu>
2014-03-12 21:50:31 -0700

    added makeD module import call

--- src/run3D.py
+++ src/run3D.py
@@ -7,6 +7,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import plotStructure
+import makeD
 
 PATH_TO_DATA = "../data/"
 IMAGE1 = 'dj/front.jpg'
@@ -17,5 +18,5 @@ img2 = cv.imread(PATH_TO_DATA + IMAGE2)
 kp1, des1 = descriptors.surf(IMAGE1)
 kp2, des2 = descriptors.surf(IMAGE2)
 matches = detection.match(img1, des1, kp1, img2, des2, kp2)
-D = makeD(matches)
+D = makeD.makeD(matches)
 plotStructure.plotStructure(D)
