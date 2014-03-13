5ebdd9d5da991ddc1a6745a6898cfe7c700cec79
Andrew Giel <agiel@stanford.edu>
2014-03-12 17:02:53 -0700

    new data

--- src/test.py
+++ src/test.py
@@ -3,8 +3,8 @@ import descriptors
 import detection
 import numpy as np
 
-img1 = cv.imread('../data/test/image1.jpg')
-img2 = cv.imread('../data/test/image2.jpg')
-kp1, des1 = descriptors.surf('test/image1.jpg')
-kp2, des2 = descriptors.surf('test/image2.jpg')
+img1 = cv.imread('../data/vase/front.jpg')
+img2 = cv.imread('../data/vase/side.jpg')
+kp1, des1 = descriptors.surf('vase/front.jpg')
+kp2, des2 = descriptors.surf('vase/side.jpg')
 matches = detection.match(img1, des1, kp1, img2, des2, kp2)
