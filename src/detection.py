b76fcf53a9c9c3f78b23f1eb9ee8a00cab141c34
Andrew Giel <agiel@stanford.edu>
2014-03-15 15:33:21 -0700

    new shoe pics

--- src/detection.py
+++ src/detection.py
@@ -48,8 +48,9 @@ def match(img1, des1, kp1, img2, des2, kp2):
 		pt2 = (int((int(pt2[0]) + width1)*xScale), int(int(pt2[1])*yScale))
 		cv.line(img3, pt1, pt2, 255)
 		
-	cv.imshow('img', img3)
 	print "... displaying matches ... "
+	cv.imshow('img', img3)
+	cv.waitKey(0)
 	return matches
 
 def matchKeypoints(des1, des2, thresh):
