1d11f4d09bee460e5384adb255f205b655652d18
Jasonvdm <jason.merwe@gmail.com>
2014-03-16 19:36:50 -0700

    added ransac 3d

--- src/ransac.py
+++ src/ransac.py
@@ -47,6 +47,40 @@ def ransac(matches, kp1, kp2):
 			model = H
 	return inliers, model
 
+def ransac3D(matches, kp1, kp2):
+	pts1 = []
+	pts2 = []
+	for match in matches:
+		ind1, ind2 = match
+		pt1 = kp1[ind1].pt
+		pt2 = kp2[ind2].pt
+		pts1.append((pt1[0], pt1[1], pt1[2]))
+		pts2.append((pt2[0], pt2[1], pt2[2]))
+	inliers = []
+	numInliers = 0	
+	model = None
+	for i in range(NUM_ITER):
+		sampleInd = random.sample(range(len(matches)), NUM_SAMPLES)
+		samples1 = np.zeros((3, NUM_SAMPLES))
+		samples2 = np.zeros((3, NUM_SAMPLES))
+		for j in range(NUM_SAMPLES):
+			samples1[:,j] = pts1[sampleInd[j]]
+			samples2[:,j] = pts2[sampleInd[j]]
+		H = np.matrix(samples2)*np.matrix(samples1).I	
+		inlierTemp = []
+		for j in range(len(pts1)):
+			x = np.matrix(pts1[j]).T
+			xPrime = H*x
+			x2 = np.matrix(pts2[j]).T
+			diff = np.linalg.norm(x2-xPrime)
+			if diff < PIXEL_THRESH:
+				inlierTemp.append(matches[j])
+		if len(inlierTemp) > numInliers:
+			numInliers = len(inlierTemp)
+			inliers = inlierTemp
+			model = H
+	return inliers, model
+
 def ransac2(matches, kp1, kp2):
 	pts1 = []
 	pts2 = []
--- src/run3D.py
+++ src/run3D.py
@@ -12,13 +12,13 @@ import makeD
 PATH_TO_DATA = '../data/'
 
 
-IMAGE1 = 'smartwatch_large/leftside.jpg'
-IMAGE2 = 'smartwatch_large/rightside.jpg'
+IMAGE1 = 'vball_small/leftside.jpg'
+IMAGE2 = 'vball_small/rightside.jpg'
 
 img1 = cv.imread(PATH_TO_DATA + IMAGE1)
 img2 = cv.imread(PATH_TO_DATA + IMAGE2)
-kp1, des1 = descriptors.sift(IMAGE1)
-kp2, des2 = descriptors.sift(IMAGE2)
+kp1, des1 = descriptors.surf(IMAGE1)
+kp2, des2 = descriptors.surf(IMAGE2)
 matches = detection.match(img1, des1, kp1, img2, des2, kp2)
 # plotStructure.plotStructure(D)
 D = makeD.makeD(matches,kp1,kp2)
